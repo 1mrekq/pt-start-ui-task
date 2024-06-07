d38b783731d57f5fea080ece17c1938434b0c18b
Oleg Ulyanov <oulyanov@ptsecurity.com>
2024-06-07 10:13:16 +0300

    fix: added KbqCommonModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { KbqCommonModule } from '@koobiq/components/core';
 
 @Component({
   standalone: true,
-  imports: [ RouterModule],
+  imports: [ RouterModule, KbqCommonModule],
   selector: 'pt-start-ui-task-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
